b0e5123981f7cc79698f8bbbe677a8b0cb1d755e
Nelfia <qdecapele@gmail.com>
2023-02-22 23:51:26 +0100

    ConnexionPage - Création validator pour confirmer le pwd + un peu de style.

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,54 +1,52 @@
-import {Component, OnDestroy} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AuthService} from "../../shared/services/auth/auth.service";
 import {CommandsService} from "../../shared/services/commands/commands.service";
-import {FormBuilder, FormGroup, ValidatorFn, Validators} from "@angular/forms";
-import {Subject, takeUntil} from "rxjs";
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Subject} from "rxjs";
 import {Router} from "@angular/router";
+import Validation from "../../utils/validation";
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
   styleUrls: ['./signin.component.scss']
 })
-export class SigninComponent implements OnDestroy {
+export class SigninComponent implements OnInit, OnDestroy {
 
-  signinForm: FormGroup;
-
+  signinForm!: FormGroup;
+  submitted: boolean = false;
   destroy$ : Subject<boolean> = new Subject<boolean>()
 
-  constructor(private fb:FormBuilder, private authService: AuthService, private router: Router, private commandeService: CommandsService) {
+  constructor(private fb:FormBuilder, private authService: AuthService, private router: Router, private commandeService: CommandsService) {}
 
+  ngOnInit() {
     this.signinForm = this.fb.group({
-      username: ['',Validators.required],
-      pwd: ['',Validators.required],
+      username: ['',[
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20)
+      ]],
+      pwd: ['',[
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(50)
+      ]],
       pwdConfirm: ['', Validators.required]
-    }, { validators: this.samePwdsValidator()});
+    },
+    { validators: [ Validation.match('pwd', 'pwdConfirm')]});
   }
-  signin() {
+
+  get f(): {[key: string]: AbstractControl} {
+    return this.signinForm.controls;
+  }
+  onSignin(): void {
+    this.submitted = true;
     const val = this.signinForm.value;
-    // TODO: Hacher le pwd & corriger le pendant côté serveur
-    console.log(val.username)
-    console.log(val.pwd)
-
-    if ((val.username && val.pwd) && (val.pwd === val.pwdConfirm)) {
-      this.authService.login(val.username, val.pwd).pipe(
-        takeUntil(this.destroy$)
-      ).subscribe((res: any) => {
-        this.commandeService.idCart = res.idCart;
-        this.router.navigateByUrl('/');
-        this.commandeService.agregateCarts(res.cart, res.user);
-      })
-    }
+    if(this.signinForm.invalid)
+      return;
+    console.log("controles ok")
   }
 
-  samePwdsValidator() : ValidatorFn {
-    return group => {
-       if (group.value.pwd === group.value.pwdConfirm){
-         return { pwdConfirm: 'Les mots de passe ne correspondent pas.'};
-       }
-       return null;
-    }
-  }
 
   ngOnDestroy() {
     this.destroy$.next(false);
